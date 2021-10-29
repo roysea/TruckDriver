26ebf7b7de8ad0dbb246756d95439e15a11c9417
Roya Salei <41925879+roysea@users.noreply.github.com>
2021-10-29 12:07:26 -0700

    Lesson 1.4 - Step into the Driver's Seat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
-
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // get transform component of the vehicle object
-        // use Translate method to change the position
+        // get player keyboard input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         
-        // to slow down the vehicle 20 meter/ second
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //use transform.Translate method to change the position
+        // set the speed 5 meter/ second * forward key
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // turn vehicle left or right
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime  );
     }
 }
